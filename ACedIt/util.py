538b4bcca224436c6535c630252c0c476a7e540b
coderick14 <bhattacharyya.rick14@gmail.com>
2017-08-15 15:20:51 +0530

    Add MAX_TRIES to get html

--- ACedIt/util.py
+++ ACedIt/util.py
@@ -379,8 +379,15 @@ class Utilities:
         """
         Utility function get the html content of an url
         """
+        MAX_TRIES = 3
         try:
-            r = rq.get(url)
+            for try_count in range(MAX_TRIES):
+                r = rq.get(url)
+                if r.status_code == 200:
+                    break
+            if try_count >= MAX_TRIES:
+                print 'Could not fetch content. Please try again.'
+                sys.exit(0)
         except Exception as e:
             print 'Please check your internet connection and try again.'
             sys.exit(0)
