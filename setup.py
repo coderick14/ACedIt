83bf5598c8b19982cf23ec64860fd05109502b43
lakshmanaram <lakshmanaram.n@gmail.com>
2017-08-17 01:06:18 +0530

    added install_entry to fix setup cache data bug

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ except ImportError:
     from distutils.core import setup
 
 import sys
-import json
-import os
+
+from ACedIt.install_entry import InstallEntry
 
 with open('requirements.txt', 'r') as f:
     requirements = [line.strip() for line in f.readlines()]
@@ -13,24 +13,6 @@ with open('requirements.txt', 'r') as f:
 extra = {}
 if sys.version_info >= (3,):
     extra['use_2to3'] = True
-
-default_site = 'codeforces'
-workdir = os.path.join(os.path.expanduser('~'), 'ACedIt')
-cache_dir = os.path.join(os.path.expanduser('~'), '.cache', 'ACedIt')
-
-from ACedIt.main import supported_sites
-
-for site in supported_sites:
-	# create cache directory and working directory structure
-    if not os.path.isdir(os.path.join(cache_dir, site)):
-        os.makedirs(os.path.join(cache_dir, site))
-    if not os.path.isdir(os.path.join(workdir, site)):
-    	os.makedirs(os.path.join(workdir, site))
-
-data = {'default_site': default_site.strip(), 'workdir': os.path.expanduser('~'),
-        'cachedir': cache_dir}
-with open(os.path.join(cache_dir, 'constants.json'), 'w') as f:
-    f.write(json.dumps(data, indent=2))
 
 setup(
     name='ACedIt',
@@ -56,6 +38,11 @@ setup(
     url='https://github.com/coderick14/ACedIt',
 
     keywords=['sample test cases', 'downloader', 'competitive programming'],
+
+    cmdclass={
+            'install': InstallEntry,
+            'develop': InstallEntry,
+    },
 
     classifiers=[
         'Operating System :: POSIX :: Linux',
